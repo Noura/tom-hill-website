e5940838c18041409af936b0d7c0ae67b69f0b5b
Noura <culurienneldoreth@gmail.com>
2013-09-22 20:15:39 -0400

    embedded example sound cloud player

--- to-deploy/static/js/app.js
+++ to-deploy/static/js/app.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
         $('.tab[data-tabname="'+tabname+'"]').addClass('active');
     });
 });
+
+window.onload = function(){
+    SC.initialize({
+        client_id: 'e6cd33d3f7b5e434c0b1d20491181fe0'
+    });
+
+    var track_url = 'http://soundcloud.com/forss/flickermood';
+    SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+        console.log('oEmbed response: ', oEmbed);
+        if (oEmbed) {
+            $('#songs').html(oEmbed.html);
+        } else {
+            $('.sidebar').addClass('hide');
+        }
+    });
+};
